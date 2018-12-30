adcd96e1ef77d3b788270d4086f50e1fc9740ded
pierremathieu18@gmail.com <mathieupierre@macbook-air-de-mathieu.home>
2018-12-30 15:18:32 +0100

    final project

--- exo_20.rb
+++ exo_20.rb
@@ -1,18 +1,24 @@
 #Construis un programme qui va demander à l'utilisateur un nombre entre 1 et 25 
 # et qui va sortir une pyramide à descendre de tant d'étages que ce nombre.
 
+user_number = 0
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
-print ">"
-user_number = gets.chomp.to_i
-pyramide = Array.new()
-n = 1
-while (n < user_number + 1)
 
-	a = "" * (user_number - n) + "#" * n
-		pyramide.push(a)
-		n = n + 1
+until user_number.between?(1,25) # pour une valeur comprise entre 1 et 25 
+	print ">>"
+	user_number = gets.chomp.to_i
+	if user_number < 1
+		puts "le nombre est trop pétit mon vieux !"
+	elsif user_number > 25
+		puts "le nombre est trop grand, il n'y a pas d'assenseur frérot !"
+	else 
+		puts "Ben voila, tu es arrivé"
+	end
+		
+end
+for n in 1.. user_number # pour chaque étage 
+	puts "" * (user_number - n) + "#" * n
 
 end
-puts pyramide
 
 
